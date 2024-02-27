b0b957c7748433430663a61ee99251cff8cda4cb
oylebirix <birioyle095@gmail.com>
2024-02-27 23:33:39 +0300

    first commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    MONGO_DB_URL:"mongodb://localhost:27017/koddemo",
+    MONGO_DB_URL:"mongodb://localhost:27017/flörtEt",
     MONGO_DB_URL_DEBUG:"",
     PORT:process.env.PORT || 3300,
     PRIVATE_KEY:"56454743358713353",
@@ -7,11 +7,11 @@ const config = {
     MIN_PASSWORD_LENGTH:6,
     AUTHORIZATION:"Bearer ",
     PROD:false,
-    ONE_SIGNAL_ID:"1ca8e627-83e7-4765-910e-cd6505b6441c",
-    ONE_SIGNAL_SECRET:"MmQ3NDI2ZTctYWZjYy00M2RjLTk1NzUtNGE4ZGE1MGY2NThk",
-    HOST:"http://188.132.130.77:3300",
-    APP_NAME:"Fist Date",
-    EMAIL: "cmpolatqx@gmail.com",
-    AUTHOR:"Fist Date",
+    ONE_SIGNAL_ID:"617b5e0d-bcee-46bb-a68a-881743fb9b5f",
+    ONE_SIGNAL_SECRET:"YmI4MTljN2ItMmU5MC00NmY1LWFmNDQtYjYxOWFhMGZlNDUz",
+    HOST:"http://78.135.85.174:3300",
+    APP_NAME:"FlörtEt",
+    EMAIL: "mylittleloveqx@gmail.com",
+    AUTHOR:"FlörtEt",
 }
 module.exports = config
